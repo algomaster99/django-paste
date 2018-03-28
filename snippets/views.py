e1a2ce2718a1c754461f73be2bd6326b950be0b6
Aman Sharma <mannu.poski10@gmail.com>
2018-03-29 00:14:32 +0530

    comments can now be added

--- snippets/views.py
+++ snippets/views.py
@@ -18,13 +18,17 @@ def create(request):
 	return render(request, 'snippets/create.html', {'form': form})
 
 def view(request, key):
+	if Snippet.objects.filter(id=key).exists():
+		snippet = Snippet.objects.get(id=key)
+	else:
+		raise Http404("Snippet doesn't exist")
+
 	if request.method == "POST":
-		return HttpResponse("Commented Yay!")
-	elif request.method == "GET":
-		if Snippet.objects.filter(id=key).exists():
-			snippet = Snippet.objects.get(id=key)
-			form = CommentForm()
-			comments = snippet.comment_set.all()
-		else:
-			raise Http404("Snippet doesn't exist")
-		return render(request, "snippets/view.html", { 'key': key, 'title': snippet.title, 'content': snippet.content, 'form': form, 'comments': comments })
+		form = CommentForm(request.POST)
+		if form.is_valid():
+			comment = form.save(commit=False)
+			comment.snippet = snippet
+			comment.save()
+	form = CommentForm()
+	comments = snippet.comment_set.order_by('-datetime_created')
+	return render(request, "snippets/view.html", { 'key': key, 'title': snippet.title, 'content': snippet.content, 'form': form, 'comments': comments })
